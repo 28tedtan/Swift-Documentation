6a05ed6478edbebaf5f534c9b7f9c1132162f8d0
Ted Tan <28tedtan@gmail.com>
2023-09-18 17:29:19 +0800

    Initial Commit

---
+++ Swift Documentation Group 4/Swift_Documentation_Group_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Documentation_Group_4App.swift
+//  Swift Documentation Group 4
+//
+//  Created by Ted Tan on 18/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Documentation_Group_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
